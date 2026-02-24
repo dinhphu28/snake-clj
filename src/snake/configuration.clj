b75fcad28626b9bc070b999f57eafcc485e573b0
Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>
2026-02-24 14:52:43 +0700

    Refactor core.clj
    - Split to multiple small files
    
    Signed-off-by: Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>

---
+++ src/snake/configuration.clj
@@ -0,0 +1,29 @@
+(ns snake.configuration)
+
+;; ============================================
+;; Configuration
+;; ============================================
+
+(def cell-size 20)
+(def board-width 30)
+(def board-height 20)
+
+;; (def move-interval 500) ;; milliseconds per snake step
+
+(def base-interval 120)
+(def min-interval 50)
+(def speed-step 5) ;; every 5 points
+(def speed-increase 10) ;; reduce 10ms each step
+
+(def directions
+  {:up    [0 -1]
+   :down  [0 1]
+   :left  [-1 0]
+   :right [1 0]})
+
+(def opposite
+  {:up :down
+   :down :up
+   :left :right
+   :right :left})
+
